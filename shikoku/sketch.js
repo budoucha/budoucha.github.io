352fba0188fb0dc127aacbb3be5deff13a2efc0a
U-BDCSURFACE\Haruto <budoucha@gmail.com>
2016-09-04 00:05:16 +0900

    trivial change

--- shikoku/sketch.js
+++ shikoku/sketch.js
@@ -12,31 +12,31 @@ function setup() {
   long = max(width, height);
   hgrid = width / 8;
   vgrid = height / 8;
-  
-  if(width < height){
-  scaleRate = width / nihon.width;
-  }else{
-  scaleRate = height / nihon.height;
+
+  if (width < height) {
+    scaleRate = width / nihon.width;
+  } else {
+    scaleRate = height / nihon.height;
   }
-  
+
   offset = -height / 2 + nihon.height / 2 * scaleRate;
-  buttonX=hgrid*2;
-  buttonY=vgrid*2
+  buttonX = hgrid * 2;
+  buttonY = vgrid * 2
   pixelDensity(1);
 }
 
 function draw() {
   background(255);
   translate(width / 2, height / 2);
-  
+
   push();
   scale(scaleRate);
-  image(nihon, 0, 0 + offset/scaleRate);
-  image(shikoku, 0, y + offset/scaleRate);
-  
+  image(nihon, 0, 0 + offset / scaleRate);
+  image(shikoku, 0, y + offset / scaleRate);
+
   pop();
 
-  image(button,buttonX,buttonY,button.width*scaleRate,button.height*scaleRate);
+  image(button, buttonX, buttonY, button.width * scaleRate, button.height * scaleRate);
 }
 
 
@@ -46,15 +46,16 @@ function windowResized() {
 
 
 function mouseClicked() {
-  if(mouseX > width/2 + buttonX - button.width*scaleRate/2
-  && mouseX < width/2 + buttonX + button.width*scaleRate/2 
-  && mouseY > height/2 + buttonY - button.height*scaleRate/2
-  && mouseY < height/2 + buttonY + button.height*scaleRate/2
-  ){
-  nanka();
-  //println(1);
+  if (mouseX > width / 2 + buttonX - button.width * scaleRate / 2 
+  && mouseX < width / 2 + buttonX + button.width * scaleRate / 2 
+  && mouseY > height / 2 + buttonY - button.height * scaleRate / 2 
+  && mouseY < height / 2 + buttonY + button.height * scaleRate / 2) 
+  {
+    nanka();
+    //println(1);
+  } else {
+    println(0);
   }
-  else{println(0);}
 }
 
 
