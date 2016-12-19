726fd6f1ac7e1fa4aace1e44b3fd15b3d100e0da
U-BDCSURFACE\Haruto <budoucha@gmail.com>
2016-12-20 02:34:23 +0900

    score now when hit

--- jimi/sketch.js
+++ jimi/sketch.js
@@ -1,14 +1,10 @@
-score = 0;
-
 function preload() {
-
   pixelDensity(1);
-
   jimiImg = loadImage("data/jimi.jpg");
-  //jimiImg = loadImage("data/dummy.jpg");
   jikiImg = loadImage("data/jiki.png");
   bulletImg = loadImage("data/bullet.png");
-  bgImg = loadImage("data/bglong.jpg");
+  bgImgLong = loadImage("data/bglong.jpg");
+  bgImgShort = loadImage("data/bg.jpg");
   aImg = loadImage("data/a.png");
   wikiLogoL = loadImage("data/wiki-Logo.png");
   wikiLogoM = loadImage("data/wiki-Logo.png");
@@ -17,49 +13,45 @@ function preload() {
 
 function setup() {
   createCanvas(600, 800);
-  short = min(width, height);
-  long = max(width, height);
   hgrid = width / 8;
   vgrid = height / 8;
-  tsize = short / 8;
-  hhalf = width / 2;
-  vhalf = height / 2;
-
-  bg = bgImg;
+  hhalf = width/2;
 
   jikiSpeedDefault = 6;
   jikiSpeedSlow = 3;
   jikiSpeed = jikiSpeedDefault;
-  shotSpeed = -10;
 
+  myBullets = new Group();
+
+  jikiImg.resize(hgrid, hgrid);
+  jiki = createSprite(hhalf, vgrid * 7, hgrid, hgrid);
+  jiki.addImage(jikiImg);
 
   jimiImg.resize(hgrid * 1.44, hgrid * 1.44);
-  jikiImg.resize(hgrid, hgrid);
-
   jimi = createSprite(hhalf, vgrid, hgrid, hgrid);
-  jiki = createSprite(hhalf, vgrid * 7, hgrid, hgrid);
   jimi.addImage(jimiImg);
-  jiki.addImage(jikiImg);
-  shtcntdwn = 0;
-
+  jimi.setCollider("rectangle");
 
   wikiLogoL.resize(hgrid * 2, hgrid * 2);
   wikiLogoM.resize(hgrid * 0.8, hgrid * 0.8);
   wikiLogoS.resize(hgrid * 0.4, hgrid * 0.4);
+
+  shtcntdwn = 0;  
+  score = 0;
+  
   url1 = "https://payments.wikimedia.org/index.php?title=Special:GlobalCollectGateway&language=ja&country=JP&currency_code=JPY&frequency=onetime&amount=Other&amountGiven=";
   id = "input_amount_other_box"
 }
 
-
 function draw() {
   update();
   background(216);
-  scroll = 0 - frameCount % bgImg.height + bgImg.height;
+  scroll = 0 - frameCount % bgImgLong.height + bgImgLong.height;
   if (scroll > 0) {
-    image(bg, 0, scroll - bgImg.height, width, bgImg.height);
+    image(bgImgLong, 0, scroll - bgImgLong.height, width, bgImgLong.height);
   }
   if (scroll < height) {
-    image(bg, 0, scroll, width, bgImg.height);
+    image(bgImgShort, 0, scroll, width, bgImgShort.height);
   }
 
   fill(0);
@@ -70,13 +62,13 @@ function draw() {
 }
 
 function update() {
-  score++;
-  jimiMove();
+  jimi.overlap(myBullets, myHit);
 
+  jimiMove();
   readKey();
 
   if (shtcntdwn > 0) {
-    shtcntdwn--; //発射可能カウントダウン
+    shtcntdwn--; //再装填カウントダウン
   }
 
 }
@@ -110,75 +102,82 @@ function readKey() {
 
 function shoot() {
   if (shtcntdwn < 1) {
-    shot = createSprite(jiki.position.x + randomGaussian(0, 5), jiki.position.y - 24, 4, 40);
-    shot.addImage(bulletImg);
-    shot.life = 80;
-    shotSpeed = -10;
-    shot.velocity.x = 0;
-    shot.velocity.y = shotSpeed;
+    myBullet = createSprite(jiki.position.x + randomGaussian(0, 5), jiki.position.y - 24, 4, 40);
+    myBullet.addImage(bulletImg);
+    myBullet.life = 80;
+    myBulletSpeed = -10;
+    myBullet.velocity.x = 0;
+    myBullet.velocity.y = myBulletSpeed;
+    myBullets.add(myBullet);
+    myBullet.setCollider("rectangle");
 
-    shtcntdwn = 3; //発射不可時間
+    shtcntdwn = 4; //再装填時間
   }
 }
+
+function myHit(jimi, myBullet) {
+  score++;
+  myBullet.remove();
+}
+
 
 function jimiMove() {
-  jimi.position.x += randomGaussian(0, 2);
+  jimi.position.x = hhalf + width / 3 * sin(frameCount / 108);
   if (frameCount % 90 == 0) {
-    jimiShot(0);
+    jimiShoot(0);
   }
-  if (frameCount % 47 == 0) {
-    jimiShot(1);
+  if (frameCount %  47 == 0) {
+    jimiShoot(1);
   }
   if (frameCount % 57 == 0) {
-    jimiShot(2);
+    jimiShoot(2);
 
   }
 }
 
-function jimiShot(type) {
+function jimiShoot(type) {
   if (type == 0) {
-    shotSpeed = 1.8;
-    shotAngle = randomGaussian(HALF_PI, 0.5);
-    shot = createSprite(jimi.position.x, jimi.position.y + 64, 4, 4);
-    shot.addImage(wikiLogoL);
-    shot.life = 440;
-    shot.velocity.x = shotSpeed * cos(shotAngle);
-    shot.velocity.y = shotSpeed * sin(shotAngle);
+    jimiShotSpeed = 1.8;
+    jimiShotAngle = randomGaussian(HALF_PI, 0.5);
+    jimiShot = createSprite(jimi.position.x, jimi.position.y + 64, 4, 4);
+    jimiShot.addImage(wikiLogoL);
+    jimiShot.life = 440;
+    jimiShot.velocity.x = jimiShotSpeed * cos(jimiShotAngle);
+    jimiShot.velocity.y = jimiShotSpeed * sin(jimiShotAngle);
   }
   if (type == 1) {
-    shotSpeed = 4;
+    jimiShotSpeed = 4;
     for (i = -1; i < 2; i += 2) {
-      shotAngle = HALF_PI - i * 0.1 - atan2(jiki.position.x - jimi.position.x, jiki.position.y - jimi.position.y);
-      shot = createSprite(jimi.position.x, jimi.position.y + 64, 4, 4);
-      shot.addImage(wikiLogoM);
-      shot.life = 180;
-      shot.velocity.x = shotSpeed * cos(shotAngle);
-      shot.velocity.y = shotSpeed * sin(shotAngle);
+      jimiShotAngle = HALF_PI - i * 0.1 - atan2(jiki.position.x - jimi.position.x, jiki.position.y - jimi.position.y);
+      jimiShot = createSprite(jimi.position.x, jimi.position.y + 64, 4, 4);
+      jimiShot.addImage(wikiLogoM);
+      jimiShot.life = 180;
+      jimiShot.velocity.x = jimiShotSpeed * cos(jimiShotAngle);
+      jimiShot.velocity.y = jimiShotSpeed * sin(jimiShotAngle);
     }
   }
   if (type == 2) {
-    shotSpeed = 8;
+    jimiShotSpeed = 8;
     for (i = 0; i < 3; i++) {
-      shotAngle = HALF_PI - atan2(jiki.position.x - jimi.position.x, jiki.position.y - jimi.position.y);
-      shot = createSprite(jimi.position.x + i * 4 * shotSpeed * cos(shotAngle), jimi.position.y + i * 4 * shotSpeed * sin(shotAngle) + 64, 4, 4);
-      shot.addImage(wikiLogoS);
-      shot.life = 80;
-      shot.velocity.x = pow(1.1, i) * shotSpeed * cos(shotAngle);
-      shot.velocity.y = pow(1.1, i) * shotSpeed * sin(shotAngle);
+      jimiShotAngle = HALF_PI - atan2(jiki.position.x - jimi.position.x, jiki.position.y - jimi.position.y);
+      jimiShot = createSprite(jimi.position.x + i * 4 * jimiShotSpeed * cos(jimiShotAngle), jimi.position.y + i * 4 * jimiShotSpeed * sin(jimiShotAngle) + 64, 4, 4);
+      jimiShot.addImage(wikiLogoS);
+      jimiShot.life = 80;
+      jimiShot.velocity.x = pow(1.1, i) * jimiShotSpeed * cos(jimiShotAngle);
+      jimiShot.velocity.y = pow(1.1, i) * jimiShotSpeed * sin(jimiShotAngle);
     }
   }
 }
 
 function drawTexts() {
-  textSize(32);
+  textSize(24);
   textAlign(RIGHT);
-  strokeWeight(4);
+  strokeWeight(3);
   stroke(0);
   fill(255);
   text("fps: " + nf(frameRate(), 0, 2) + " ", width, height - vgrid / 2);
   text("DONATE: ￥" + score + " ", width, vgrid / 2);
 }
-
 
 
 function donate() { //寄付ページに移動
