a8b585b8ec58ae6de96d19624ed2274ee3257805
U-BDCSURFACE\Haruto <budoucha@gmail.com>
2017-01-02 22:25:11 +0900

    now probably works on smartphones

--- saisen/sketch.js
+++ saisen/sketch.js
@@ -2,43 +2,57 @@ function preload() {
   pixelDensity(1);
   coinImg = loadImage("data/money_5a.png");
   boxImg = loadImage("data/saisenbako.png");
+  tutoImg = loadImage("data/tuto.png");
   s1 = loadSound('data/money-drop2.mp3');
 }
 
 function setup() {
+  touch = false;
   n = allSprites.length;
   for (i = 0; i < n; i++) {
     allSprites[0].remove();
   }
-  var myCanvas = createCanvas(windowWidth, windowHeight * 0.8);
+  var myCanvas = createCanvas(windowWidth * 0.96, windowHeight * 0.75);
   myCanvas.parent('sketch-holder');
 
   coinImg.resize(height / 12, height / 12);
   boxImg.resize(boxImg.width * height / 4 / boxImg.height, height / 4);
-
+  if(tutoImg.width>width*0.8){
+  tutoImg.resize(width * 0.8, tutoImg.height * width * 0.8 / tutoImg.width)
+  }
   saisens = new Group();
   saisenbako = createSprite(width / 2, height / 4, 1, 1);
   saisenbako.addImage(boxImg);
 
+  useQuadTree(true);
+
   s1.setVolume(0.1);
   s1.playMode('sustain');
 
+  imageMode(CENTER);
+
   kingaku = 0;
+  bg = 255;
 }
 
 function draw() {
-  background(255);
-  if (mouseIsPressed || keyDown("SPACE")) {
+  background(bg);
+  if (kingaku === 0) {
+    image(tutoImg, width / 2, height / 2);
+  }
+
+  if (touch || keyDown("SPACE")) {
     throwSaisen();
   }
-  drawSprites();
 
   saisenbako.overlap(saisens, charin);
+
+  drawSprites();
 
   fill(0);
   textSize(24);
   textAlign(CENTER);
-  text('￥' + kingaku, width / 2, height * 15 / 16);
+  text('￥' + kingaku, width / 2, height * 0.8);
 }
 
 function throwSaisen() {
@@ -46,8 +60,8 @@ function throwSaisen() {
   saisen.addImage(coinImg);
   saisen.life = 64;
   angle = HALF_PI - atan2(saisenbako.position.x - mouseX, saisenbako.position.y - mouseY);
-  saisen.velocity.x = height / 12 * cos(angle);
-  saisen.velocity.y = height / 12 * sin(angle);
+  saisen.velocity.x = height / 16 * cos(angle);
+  saisen.velocity.y = height / 16 * sin(angle);
   saisen.setCollider("circle", 0, 0, saisen.width / 2.1);
   saisens.add(saisen);
 }
@@ -56,4 +70,16 @@ function charin(saisenbako, saisen) {
   s1.play();
   saisen.remove();
   kingaku += 5;
+}
+
+
+function touchStarted() {
+  touch = true;
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    return false;
+  }
+}
+
+function touchEnded() {
+  touch = false;
 }
