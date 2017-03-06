15e0d20358f64b7c1e98ea9a6d98847ad6f6fd55
Haruto Kanno <budoucha@gmail.com>
2017-03-07 03:55:21 +0900

    code seikei

--- kuso/sketch.js
+++ kuso/sketch.js
@@ -2,8 +2,8 @@ var htuImage, handClapSound;
 var bg, tSize, hasClapped;
 
 function preload() {
-  htuImage = loadImage("assets/HTU.png")
-  handClapSound = loadSound("assets/hall-clapping-hands1.mp3");
+    htuImage = loadImage("assets/HTU.png")
+    handClapSound = loadSound("assets/hall-clapping-hands1.mp3");
 }
 
 function setup() {
@@ -23,15 +23,15 @@ function draw() {
     background(bg);
     imageMode(CENTER);
     //copy(htuImage,0,0,htuImage.width,htuImage.height,width*0.2 , height*0.2 , width*0.6,height*0.6);
-    image(htuImage,width/2,height/2);
+    image(htuImage, width / 2, height / 2);
 
-    if(accelerationZ > 30){
-        if(hasClapped === false ){
+    if (accelerationZ > 30) {
+        if (hasClapped === false) {
             handClap();
             hasClapped = true;
         }
     }
-    else{
+    else {
         hasClapped = false;
         tSize = 64;
     }
@@ -44,15 +44,15 @@ function mousePressed(){
 }*/
 
 
-function handClap(){
+function handClap() {
     handClapSound.play();
     tSize = 128;
 }
 
 
-function drawHands(){ 
+function drawHands() {
     textSize(tSize);
     textAlign(CENTER);
     noStroke();
-    text("👏",width/2,height/2);
+    text("👏", width / 2, height / 2);
 }
