3fa93322a2f17775261917864b8cfc6d6ebfdfa5
U-BDCSURFACE\Haruto <budoucha@gmail.com>
2017-03-15 19:11:43 +0900

    added max and min

--- accelerometer/sketch.js
+++ accelerometer/sketch.js
@@ -1,3 +1,7 @@
+var aX, aY, aZ;
+var aX_max, aY_max, aZ_max;
+var aX_min, aY_min, aZ_min;
+
 function setup() {
   createCanvas(windowWidth, windowHeight, P2D);
 
@@ -6,20 +10,32 @@ function setup() {
   hgrid = width / 8;
   du = min(vgrid, hgrid);
 
-  tsize = du / 2;
+  tsize = du / 3;
   rate = width / 180;
   rectH = tsize / 2;
-  
+
   pixelDensity(1);
+
+  aX_max = 0; aY_max = 0; aZ_max = 0;
+  aX_min = 0; aY_min = 0; aZ_min = 0;
 }
 
 function draw() {
   aX = accelerationX;
   aY = accelerationY;
   aZ = accelerationZ;
+
+  aX_max = (aX > aX_max) ? aX : aX_max;
+  aY_max = (aY > aY_max) ? aY : aY_max;
+  aZ_max = (aZ > aZ_max) ? aZ : aZ_max;
+
+  aX_min = (aX < aX_min) ? aX : aX_min;
+  aY_min = (aY < aY_min) ? aY : aY_min;
+  aZ_min = (aZ < aZ_min) ? aZ : aZ_min;
+
   background(0);
   rectMode(CORNERS);
-  textAlign(CENTER, CENTER);
+  textAlign(CENTER, BOTTOM);
   textSize(tsize);
 
   translate(width / 2, height / 2);
@@ -28,15 +44,15 @@ function draw() {
 
 
   fill("#FF0000");
-  text(nfp(aX, 3, 2) + " m/s/s", 0, -vgrid);
+  text("X: " + nfp(aX, 3, 2) + " m/s^2\nmax: " + nfp(aX_max, 3, 2) + " m/s^2   min: " + nfp(aX_min, 3, 2) + " m/s^2", 0, -vgrid);
   rect(aX * rate * (aX < 0), -vgrid + tsize - rectH, aX * rate * (aX > 0), -vgrid + tsize + rectH);
 
   fill("#00FF00");
-  text(nfp(aY, 3, 2) + " m/s/s", 0, 0);
+  text("Y: " + nfp(aY, 3, 2) + " m/s^2\nmax: " + nfp(aY_max, 3, 2) + " m/s^2   min: " + nfp(aY_min, 3, 2) + " m/s^2", 0, 0);
   rect(aY * rate * (aY < 0), tsize - rectH, aY * rate * (aY > 0), tsize + rectH);
 
   fill("#0000FF");
-  text(nfp(aZ, 3, 2) + " m/s/s", 0, vgrid);
+  text("Z: " + nfp(aZ, 3, 2) + " m/s^2\nmax: " + nfp(aZ_max, 3, 2) + " m/s^2   min: " + nfp(aZ_min, 3, 2) + " m/s^2", 0, vgrid);
   rect(aZ * rate * (aZ < 0), vgrid + tsize - rectH, aZ * rate * (aZ > 0), vgrid + tsize + rectH);
 
   fill(255);
