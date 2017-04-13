c51f38145dc17d7e5269748609b1a10bd5566d57
Haruto Kanno <budoucha@gmail.com>
2017-04-14 01:59:26 +0900

    improved performance

--- rainbow/sketch.js
+++ rainbow/sketch.js
@@ -1,27 +1,24 @@
 var img;
 function preload() {
-    pixelDensity(1);
-    img = loadImage("assets/hima.png");
+  pixelDensity(1);
+  img = loadImage("assets/hima.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  short = min(width, height);
-  long = max(width, height);
   colorMode(HSB);
   imageMode(CENTER);
 }
 
 function draw() {
-    background(0);
-    for(i =0;i<width;i++){
-      strokeWeight(1);
-      stroke((i+frameCount*2)%width*360/width,100,100);
-      line(i,0,i,height);
-    }
-    image(img,width/2,height/2);
+  weight = width / 360;
+  strokeWeight(weight);
+  for (i = 0; i < 360; i++) {
+    stroke((i + frameCount * 5) % 360, 100, 100);
+    line(i * weight, 0, i * weight, height);
+  }
+  image(img, width / 2, height / 2);
 }
-
 
 
 function windowResized() {
