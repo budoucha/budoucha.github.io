40d82fc4f8667bf42d29e4cfea83a8094ea0637c
Haruto Kanno <budoucha@gmail.com>
2017-04-14 02:05:34 +0900

    deleted weird hima

--- rainbow/sketch.js
+++ rainbow/sketch.js
@@ -1,13 +1,10 @@
-var img;
 function preload() {
   pixelDensity(1);
-  img = loadImage("assets/hima.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
-  imageMode(CENTER);
 }
 
 function draw() {
@@ -17,7 +14,6 @@ function draw() {
     stroke((i + frameCount * 5) % 360, 100, 100);
     line(i * weight, 0, i * weight, height);
   }
-  image(img, width / 2, height / 2);
 }
 
 
